bc8681b6c46f7799622668bff7f569c2bf625023
santiagoss01 <santiagozehcnas@gmail.com>
2023-01-19 20:32:02 +0000

    My first commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,7 +5,35 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
 api = Blueprint('api', __name__)
+
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test@test.com" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    print(access_token)
+    response_body = { "email": email,
+                      "access token": access_token}
+
+    return jsonify(access_token=access_token)
+
+@api.route("/private", methods=["GET"])
+@jwt_required() 
+def protected():
+# Access the identity of the current user with get jwt identity
+  current_user = get_jwt_identity ()
+  response_body = {"email": current_user,
+                   "message": "loggin ok"}
+  return jsonify (response_body), 200
+
 
 
 @api.route('/hello', methods=['POST', 'GET'])
